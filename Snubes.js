35ed0f7733464360149f64b60271f637c0422d46
Sisir Madanu <64269215+sisirmadanu@users.noreply.github.com>
2020-11-27 03:06:15 +0100

    Update Snubes.js

--- Snubes.js
+++ Snubes.js
@@ -13,11 +13,11 @@ describe('Show Password', function(){
         cy.get('[placeholder="example@emailaddress.com"]').click() //When user clicks on "emailaddress" text field
         cy.get('[placeholder="example@emailaddress.com"]').type('test@gmail.com') //When user enters emailaddress      
         cy.get('[placeholder="Password"]').click() //When user clicks on "password" text field      
-        cy.get('[placeholder="Password"]').type('Qwerty@123') //When user enters password      
-        cy.contains('Show').click() //When user clicks on "Show" password       
+        cy.get('[placeholder="Password"]').type('Qwerty@123') //When user enters password        
      })
     
     it('SHOW PASSWORD VALIDATION',function(){
+            cy.contains('Show').click() //When user clicks on "Show" password
             cy.get('[placeholder="Password"]').click() // When user is able to view password    
             cy.get('[name="password"]').should('have.value','Qwerty@123') // When user validates the password
             
