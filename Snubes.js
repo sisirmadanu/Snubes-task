715c7bb029e2634751e9eee790af719d8d0c2c19
Sisir Madanu <64269215+sisirmadanu@users.noreply.github.com>
2020-11-27 02:35:35 +0100

    Update Snubes.js

--- Snubes.js
+++ Snubes.js
@@ -1,24 +1,26 @@
 describe('Show Password', function(){
+    
     it('VISIT PAGE', function(){
-cy.visit('https://www.snubes.com/')  //When user visits home page
+        cy.visit('https://www.snubes.com/')  //When user visits home page
     })
     
-    it('WHEN USER CLICKS ON SIGNIN',function(){
-        cy.contains('Sign In').click()  //When user clicks on Sign In Button from Home page.
-        cy.get('h4').contains('Sign In')  //When User Validates Sign In popup window.
-        
-    })
+    it('WHEN USER CLICKS ON SIGN IN',function(){
+        cy.contains('Sign In').click()  //When user clicks on Sign In Button from Home page
+        cy.get('h4').contains('Sign In')  //When User Validates Sign In popup window      
+     })
+    
     it('WHEN USER ENTERS USERNAME & PASSWORD',function(){
-        cy.get('[placeholder="example@emailaddress.com"]').click()//When user clicks on "emailaddress" text field
-        cy.get('[placeholder="example@emailaddress.com"]').type('test@gmail.com')//When user enters emailaddress      
-        cy.get('[placeholder="Password"]').click()//When user clicks on "password" text field.      
+        cy.get('[placeholder="example@emailaddress.com"]').click() //When user clicks on "emailaddress" text field
+        cy.get('[placeholder="example@emailaddress.com"]').type('test@gmail.com') //When user enters emailaddress      
+        cy.get('[placeholder="Password"]').click() //When user clicks on "password" text field      
         cy.get('[placeholder="Password"]').type('Qwerty@123') //When user enters password      
-        cy.contains('Show').click()//When user clicks on "Show" password       
-           })
-     it('SHOW PASSWORD VALIDATION',function(){
-            cy.get('[placeholder="Password"]').click()// When user is able to view password    
-            cy.get('[name="password"]').should('have.value','Qwerty@123')// When user validates the password
+        cy.contains('Show').click() //When user clicks on "Show" password       
+     })
+    
+    it('SHOW PASSWORD VALIDATION',function(){
+            cy.get('[placeholder="Password"]').click() // When user is able to view password    
+            cy.get('[name="password"]').should('have.value','Qwerty@123') // When user validates the password
             
-        })
+     })
            
 })
