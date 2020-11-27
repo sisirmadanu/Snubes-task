7e0edbd1509b1bb7d9db84c64b2b308d4d32dac3
Sisir Madanu <64269215+sisirmadanu@users.noreply.github.com>
2020-11-27 02:17:46 +0100

    Add files via upload

---
+++ Snubes.js
@@ -0,0 +1,31 @@
+describe('Show Password', function(){
+    it('VISIT PAGE', function(){
+cy.visit('https://www.snubes.com/')
+    })
+    /*it('SELECT LANGUAGE',function(){
+        cy.contains('EN').click()
+        cy.get('.top_header_language_link').contains('EN')
+    })*/
+    it('WHEN USER CLICKS ON SIGNIN',function(){
+        cy.contains('Sign In').click()  //When user clicks on Sign In Button from Home page.
+        cy.get('h4').contains('Sign In')  //When User Validates Sign In popup window is open.
+        
+    })
+    it('WHEN USER ENTERS USERNAME & PASSWORD',function(){
+        cy.get('[placeholder="example@emailaddress.com"]').click()//When user clicks on "emailaddress" text field
+        cy.get('[placeholder="example@emailaddress.com"]').type('test@gmail.com')//When user enters emailaddress
+        cy.wait(2000)
+        cy.get('[placeholder="Password"]').click()//When user clicks on "password" text field.
+        cy.wait(2000)
+        cy.get('[placeholder="Password"]').type('Qwerty@123') //When user enters password
+        cy.wait(2000)
+        cy.contains('Show').click()//When user clicks on "Show" password
+        cy.wait(5000) 
+           })
+           it('SHOW PASSWORD VALIDATION',function(){
+            cy.get('[placeholder="Password"]').click()// When user is able to view password    
+            cy.get('[name="password"]').should('have.value','Qwerty@123')// When user validates the password
+            
+        })
+           
+})
